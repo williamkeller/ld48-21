4484fc9cdbcb85c317c6b9b32b6a632925a38324
William Keller <william@arda.net>
2011-08-20 00:52:09 -0400

    Switched to 32 pixel tile, walls and grid graphics

--- main.rb
+++ main.rb
@@ -3,29 +3,62 @@ require "gosu"
 
 class GameWindow < Gosu::Window
   
-  SCREEN_X = 800
-  SCREEN_Y = 600
+  SCREEN_X = 640
+  SCREEN_Y = 480
+  BLIT_SPEED = 3
   
   def initialize
     super SCREEN_X, SCREEN_Y, false
-    caption = "Escape deletion"
-    
-    @blank = Gosu::Image.new self, "media/images/blank.png", true
+    self.caption = "Escape deletion"
+
+    @grid = Gosu::Image.new self, "media/images/grid.png", true
+    @wall1 = Gosu::Image.new self, "media/images/wall-1.png", true
+    @blit = Gosu::Image.new self, "media/images/blit.png", true
     @scroll_offset = 0
+    
+    @player_x = 400
+    @player_y = 300
   end
   
   
   def update
-    @scroll_offset = (@scroll_offset + 1) % 64
+    @scroll_offset = (@scroll_offset + 1) % 32
+    
+    if button_down? Gosu::KbLeft
+      @player_x -= BLIT_SPEED
+    end
+    
+    if button_down? Gosu::KbRight
+      @player_x += BLIT_SPEED
+    end
+    
+    if button_down? Gosu::KbUp
+      @player_y -= BLIT_SPEED
+    end
+    
+    if button_down? Gosu::KbDown
+      @player_y += BLIT_SPEED
+    end
+
   end
   
   
   def draw
-    (2..10).each do |x|
-      (-1..10).each do |y|
-        @blank.draw x * 64, y * 64 + @scroll_offset, 0
+    (0..20).each do |x|
+      (0..20).each do |y|
+        @grid.draw x * 32, y * 32 + @scroll_offset, 0
       end
     end
+    (1..20).each do |y|
+      @wall1.draw 64, y * 32, 0
+      @wall1.draw 576, y * 32, 0
+    end
+    
+    @blit.draw @player_x, @player_y, 1
+  end
+  
+  def button_down(key_id)
+    close if key_id == Gosu::KbEscape
   end
   
 end
